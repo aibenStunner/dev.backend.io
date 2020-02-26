ba89617341ae87df131d2ff13875d4f9a36b8e24
Kevin Otuteye <kevinotuteye@gmail.com>
2020-02-26 19:30:14 +0000

    Fetch and store image implemented

--- index.js
+++ index.js
@@ -2,6 +2,19 @@ const express = require("express");
 const app = express();
 const port = 2000;
 
-app.get("/", (req, res) => res.send("God's eye backend test!"));
+const fs = require("fs");
+const fetch = require("node-fetch");
+
+fetch(
+  "https://www.google.com/images/branding/googlelogo/2x/googlelogo_color_92x30dp.png"
+).then(res => {
+  const dest = fs.createWriteStream("./public/pic.jpg");
+  res.body.pipe(dest);
+  console.log("Saved!");
+});
+
+app.use(express.static("public"));
+
+app.get("/", (req, res) => res.send(`<img src='pic.jpg'/>`));
 
 app.listen(port, () => console.log(` listening on port ${port}!`));
