450028341ba9e72988009253565b5d6c4b4f50ec
Kevin Otuteye <kevinotuteye@gmail.com>
2020-02-28 11:29:18 +0000

    Trying http request stream

--- index.js
+++ index.js
@@ -1,20 +1,12 @@
 const express = require("express");
 const app = express();
-const port = 2000;
-
 const fs = require("fs");
 const fetch = require("node-fetch");
+const http = require("http");
+const port = 2000;
 
-fetch(
-  "https://www.google.com/images/branding/googlelogo/2x/googlelogo_color_92x30dp.png"
-).then(res => {
-  const dest = fs.createWriteStream("./public/pic.jpg");
-  res.body.pipe(dest);
-  console.log("Saved!");
+app.get("/", (req, res) => {
+  
 });
-
-app.use(express.static("public"));
-
-app.get("/", (req, res) => res.send(`<img src='pic.jpg'/>`));
 
 app.listen(port, () => console.log(` listening on port ${port}!`));
