6c33a60dffc368c47a1b5d71ea4aa43718954c7c
Kevin Otuteye <kevinotuteye@gmail.com>
2020-03-03 08:06:26 +0000

    Implemented and tested rot13 link encryption

--- index.js
+++ index.js
@@ -1,12 +1,50 @@
 const express = require("express");
 const app = express();
-const fs = require("fs");
-const fetch = require("node-fetch");
-const http = require("http");
-const port = 2000;
+const port = 4000;
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const SimpleCrypto = require("simple-crypto-js").default;
+const cameras = require("./store").cameras;
 
-app.get("/", (req, res) => {
-  
+//Setting up middlewares
+app.use(cors());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+//Returns dynamic secret for encryption based on current date
+function getSecret() {
+  return new Date().getDate().toString();
+}
+
+//Return encrypted link
+function encryptLink(camera_name) {
+  let encryptor = new SimpleCrypto(getSecret());
+  return encryptor.encrypt(cameras[camera_name]);
+}
+
+// This function decrypts the encrypted link. Only for testing purposes
+// function decryptLink(secret, crypt) {
+//   let decryptor = new SimpleCrypto(secret);
+//   return decryptor.decrypt(crypt);
+// }
+
+//GET Handlers
+app.get("/stream", (req, res) => {
+  res.json(encryptLink("cam_1") + getSecret());
 });
 
-app.listen(port, () => console.log(` listening on port ${port}!`));
+// WARNING: Do not uncomment in production!
+// app.get("/decrypt", (req, res) => {
+//   res.send(decryptLink(req.query.date, req.query.auth));
+// });
+
+//POST Handlers
+app.post("/feed", (req, res) => {
+  console.log(req.body);
+  res.json("Received");
+});
+
+//Listener
+app.listen(port, () => {
+  console.log(`dev.io - Godseye running on port ${port}`);
+});
---
+++ store.js
@@ -0,0 +1,9 @@
+const cameras = {
+  cam_1: "http://wmccpinetop.axiscam.net/mjpg/video.mjpg",
+  cam_2: "http://208.72.70.172/mjpg/video.mjpg",
+  cam_3: "http://158.58.130.148/"
+};
+
+module.exports = {
+  cameras: cameras
+};
