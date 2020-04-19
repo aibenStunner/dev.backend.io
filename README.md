b57d7d73cef10d64ec8a7c9f958b5c230613153a
knotuteye <kevinotuteye@gmail.com>
2020-04-19 21:09:53 +0000

    Let there be light

--- README.md
+++
@@ -1,3 +0,0 @@
-# dev.backend.io
-
-Supporting the Web and Mobile developers with sustainable and scalable database solutions.
--- getHandlers.js
+++
@@ -1,7 +0,0 @@
-const path = require('path');
-
-module.exports = {
-  get_404() {
-    return path.join(__dirname + '/resx/404.html');
-  }
-};
