c328d4ec862bc1e27a205539eb0eb0b3bc151b06
knotuteye <kevinotuteye@gmail.com>
2020-04-19 22:59:37 +0000

    created api skeleton

--- index.js
+++ index.js
@@ -2,11 +2,18 @@ const express = require('express')
 const app = express()
 const port = 5000
 
-/**@summary This handles all GET requests to the server */
+// GET Handler
 app.get('/*', (req, res) =>
 	res.sendFile('/public/index.html', { root: __dirname }, (err) =>
 		console.error(err)
 	)
 )
+
+// POST Handlers 
+app.post('login', (req, res) => {})
+
+app.post('logout', (req, res) => {})
+
+app.post('feed', (req, res) => {})
 
 app.listen(port, console.log(`Godseye Server live on port ${port}`))
