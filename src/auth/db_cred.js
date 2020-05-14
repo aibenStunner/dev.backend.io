a53c904958502af2af257a058eda12c496d20280
Kevin Otuteye <kevinotuteye@gmail.com>
2020-05-14 11:33:44 +0000

    Fixed RDS DB name

--- src/auth/db_cred.js
+++ src/auth/db_cred.js
@@ -4,6 +4,6 @@ module.exports = {
 		user: process.env.RDS_USERNAME,
 		password: process.env.RDS_PASSWORD,
 		port: process.env.RDS_PORT,
-		database: RDS_DB_NAME,
+		database: process.env.RDS_DB_NAME,
 	},
 }
