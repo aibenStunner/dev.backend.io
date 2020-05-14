76693ef972fa0053b84dc7fbea68d88db1f86ac4
Kevin Otuteye <kevinotuteye@gmail.com>
2020-05-14 13:41:41 +0000

    fixed linking

--- src/camera_ops/getUserCameras.js
+++ src/camera_ops/getUserCameras.js
@@ -2,8 +2,8 @@
  * @author Kevin Nene Otuteye
  */
 const mysql = require('mysql')
-const DBMeta = require('./db_cred')
-const hashSuite = require('./password_hasher')
+const DBMeta = require('../auth/db_cred')
+const hashSuite = require('../auth/password_hasher')
 /**
  *
  * @param {String} parentId
@@ -17,22 +17,10 @@ function getUserCameras(parentId, callback) {
 		`SELECT cameraId,camera_name,camera_link from parent_ward_class WHERE parentId='${parentId}'`,
 		function (error, results, fields) {
 			console.log(results)
-
 			if (error) throw error
 			if (!results[0]) {
 				connection.end()
 				return callback(undefined)
-			} else {
-				hashSuite.comparePassword(
-					password,
-					results[0].password,
-					(isMatch) => {
-						connection.end()
-						return isMatch
-							? callback(results[0])
-							: callback(undefined) //isn't she beautiful
-					}
-				)
 			}
 		}
 	)
